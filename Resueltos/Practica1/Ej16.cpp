2381f4dd322f160066968f8233dc9559c291379f
mlakowsky <mlakowsky@gmail.com>
2023-04-16 23:18:42 -0300

    16

--- Resueltos/Practica1/Ej16.cpp
+++ Resueltos/Practica1/Ej16.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
+vector<int> V;
+vector<pair<int,int>> E;
+
+void fs(vector<int> &S, vector<bool> &W) {
+    if (W.size() == 0) return;
+
+    int best = 0;
+    vector<int> bestS;
+
+    for (int i = 0; i < W.size(); ++i) {
+        S.push_back(W[i]);
+        for (int j = 0; j < W.size(); ++j) {
+            
+        }
+    }
+}
+
 int main() {
+
     return 0;
 }
